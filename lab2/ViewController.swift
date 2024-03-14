0b0fd03737fd7890214017cea268ff3467ac3cee
maximu211 <63352037+maximu211@users.noreply.github.com>
2024-03-14 23:44:34 +0200

    Add files via upload

---
+++ lab2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lab2
+//
+//  Created by Максим on 3/14/24.
+//  Copyright © 2024 Максим. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
